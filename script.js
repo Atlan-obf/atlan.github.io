6e29a20755bfb38c78cc5077e58022e556e340ef
Ares <tomislavvrancik@gmail.com>
2025-09-26 15:32:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,14 @@ window.addEventListener("message", function(event) {
     const data = event.data;
 
     if (data.type === "updatePlayerInfo") {
-        if (data.name) document.getElementById("player-name").textContent = data.name;
-        if (data.keybind) document.getElementById("player-keybind").textContent = data.keybind;
-        if (data.id) document.getElementById("player-id").textContent = data.id;
-    }
-
-    if (data.type === "debugLog") {
-        const debugContent = document.getElementById("debug-content");
-        let p = document.createElement("p");
-        p.textContent = "> " + data.message;
-        if (data.level) p.classList.add(data.level); 
-        debugContent.appendChild(p);
-        debugContent.scrollTop = debugContent.scrollHeight;
-    }
-
-    if (data.type === "toggleUI") {
-        const dbg = document.getElementById("debug-window");
-        if (data.visible) {
-            dbg.style.display = "block";
-        } else {
-            dbg.style.display = "none";
+        if (data.name) {
+            document.getElementById("player-name").textContent = data.name;
+        }
+        if (data.keybind) {
+            document.getElementById("player-keybind").textContent = data.keybind;
+        }
+        if (data.id) {
+            document.getElementById("player-id").textContent = data.id;
         }
     }
 });
