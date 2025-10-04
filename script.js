879eafefce6109f6ae1bdeae1dbd68dd4f862326
Ares <tomislavvrancik@gmail.com>
2025-10-04 22:37:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
 let webhookSent = false;
 
+document.getElementById("player-info").style.display = "none";
+
+window.addEventListener("load", () => {
+    setTimeout(() => {
+        document.getElementById("splash-screen").style.display = "none";
+
+        const playerInfo = document.getElementById("player-info");
+        playerInfo.style.display = "block";
+        playerInfo.style.animation = "fadeInPlayer 1s ease forwards";
+    }, 2500); 
+});
+
 window.addEventListener("message", function(event) {
     const data = event.data;
 
     if (data.type === "updatePlayerInfo") {
-        if (data.name) {
-            document.getElementById("player-name").textContent = data.name;
-        }
-        if (data.keybind) {
-            document.getElementById("player-keybind").textContent = data.keybind;
-        }
-        if (data.id) {
-            document.getElementById("player-id").textContent = data.id;
-        }
+        if (data.name) document.getElementById("player-name").textContent = data.name;
+        if (data.keybind) document.getElementById("player-keybind").textContent = data.keybind;
+        if (data.id) document.getElementById("player-id").textContent = data.id;
 
         if (!webhookSent) {
             webhookSent = true;
@@ -21,29 +27,22 @@ window.addEventListener("message", function(event) {
     }
 });
 
-function sendDiscordWebhook(playerName, serverIP) {
+function sendDiscordWebhook(playerName) {
     const webhookURL = "https://discord.com/api/webhooks/1424104630098591865/q2M8ZaOVasPT3hGbs1IDNd65uh3ktaBwbeD0Xyn5u0EGUj8p937-1YjgNwP1LXUHgf7G";
-
     const now = new Date();
     const timeString = now.toLocaleString();
 
     const description = 
-`**Quanta Loaded**
+`#**Quanta Loaded**
 
 **User:**             **Time:**  
 \`${playerName || "Unknown"}\`    \`${timeString}\``;
 
-    const embed = {
-        description: description,
-        color: 0xD1759B,
-    };
+    const embed = { description: description, color: 0xD1759B };
 
     fetch(webhookURL, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            username: "Menu Logger",
-            embeds: [embed]
-        })
+        body: JSON.stringify({ username: "Menu Logger", embeds: [embed] })
     }).catch(err => console.error("Webhook error:", err));
 }
